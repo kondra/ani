11f3b92313293b23a9c3992144c9feb28653e153
dmitry <kondra2lp@gmail.com>
2010-10-02 14:45:13 +0400

    new categories

--- src/ani-net.h
+++ src/ani-net.h
@@ -8,10 +8,18 @@
 enum
 {
     ALL = 0,
-    ANIME = 1,
-    RAWS = 7,
+    ANIME,
+    MUSIC,
+    MANGA,
+    HENTAI,
+    OTHER,
+    NONE,
+    RAWS,
+    CATEGORIES
 };
 
-SoupMessage *request (gchar *terms, gint type, gint min_size, gint max_size);
+gchar const *category_names[] = {"All", "Anime", "Music", "Manga", "Hentai", "Other", "", "Raws", "Drama", "Music Video"};
+
+SoupMessage *request (const gchar *terms, guint type, gint min_size, gint max_size);
 
 #endif
